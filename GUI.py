3281fc77341751c9f5455e194a7b2881aff97e74
gelina <gelina@pc5403i.lan.esiee.fr>
2018-01-12 15:04:38 +0100

    fix map

--- GUI.py
+++ GUI.py
@@ -8,6 +8,7 @@ Created on Jan 02 17:15:42 2017
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.mlab as mlab
+from matplotlib import cm as cmap
 import matplotlib.pyplot as plt
 
 
@@ -27,15 +28,15 @@ def build_map(dico):
   #   MY_MAP = Basemap(lon_0=2.579747,lat_0=48.8411547,width=9999,height=9999,
   # resolution='c', projection='merc', epsg=27571)
 
- #     MY_MAP = Basemap(lon_0=2.344589,lat_0=48.851824,width=9999,height=9999,
- # rsphere=(6378137.00, 6356752.3142), resolution='i', projection='merc',area_thresh = 0.1, epsg=27571)
+ #    MY_MAP = Basemap(lon_0=2.344589,lat_0=44.851824,width=99999,height=99999,
+ # rsphere=(6378137.00, 6356752.3142), resolution='l', projection='merc',area_thresh = 0.1, epsg=27571)
 
 
-    # MY_MAP = Basemap(llcrnrlon=1.450561, llcrnrlat=48.136375, urcrnrlon=3.550762, urcrnrlat=49.240305,
- # rsphere=(6378137.00, 6356752.3142), resolution='i', projection='merc',area_thresh = 0.1, epsg=27571)
+    MY_MAP = Basemap(llcrnrlon=-0.5, llcrnrlat=48.5, urcrnrlon=0.5, urcrnrlat=49.240305,
+ rsphere=(6378137.00, 6356752.3142), resolution='l', projection='merc',area_thresh = 0.1, epsg=27571)
 
-    MY_MAP = Basemap(llcrnrlon=-2.5, llcrnrlat=45, urcrnrlon=2.5, urcrnrlat=50,
- rsphere=(6378137.00, 6356752.3142), resolution='i', projection='merc',area_thresh = 0.1, epsg=27571)
+ #    MY_MAP = Basemap(llcrnrlon=-2.5, llcrnrlat=45, urcrnrlon=2.5, urcrnrlat=50,
+ # rsphere=(6378137.00, 6356752.3142), resolution='l', projection='merc',area_thresh = 0.1, epsg=27571)
     # les coordonnées sont centrées sur l'ile de France sur un e projection mercator. on peut faire varier la resolution de l'image générée
 
     # UTILISER DES SHAPEFILES OU PLUS SIMPLEMENT UTILISER UNE VUE EN LIGNE TYPE GMAPS OU OPENSTREETMAPS SERAIT PLUS PERTINENT
@@ -45,9 +46,24 @@ def build_map(dico):
     
     # MY_MAP.drawparallels()
     # MY_MAP.drawmeridians()
-    
+    lon = []
+    lat = []
+    poids = []
+    for station in dico:
+        poids.append(station[0])
+        lon.append(station[1][1])
+        lat.append(station[1][0])
+    X_COORD, Y_COORD = MY_MAP(lon, lat)
+    CMAP = plt.cm.get_cmap('Oranges')
+
+
+
+
     MY_MAP.drawrivers()
-    MY_MAP.arcgisimage(xpixels =1500 , verbose = True)
+    MY_MAP.arcgisimage(xpixels =1600 , verbose = True, service= 'ESRI_StreetMap_World_2D')
+    heatmap, xedges, yedges = np.histogram2d(lon, lat, bins=200)
+    SCA = MY_MAP.scatter(X_COORD, Y_COORD, s=100, marker='o', c=poids, cmap=CMAP)
+    plt.colorbar(SCA)
     plt.show()
 
 
--- run.py
+++ run.py
@@ -14,16 +14,16 @@ import GUI
 
 
 def main():
-    # dic = projet.valid_parstation_parjour('validations.csv')
-    # l= projet.weekdaydetection(dic)
-    # moy = projet.moyennesurannee(l[0])
+    dic = projet.valid_parstation_parjour('validations.csv')
+    l= projet.weekdaydetection(dic)
+    moy = projet.moyennesurannee(l[0])
     # l = projet.split_hist_data(moy, 12000)
     
     #GUI.build_histo(l[0])
     #GUI.build_histo(l[1])
-    # geo = projet.build_stations_coordonates("emplacement-des-gares-idf.csv")
-    # md = projet.build_map_data(geo,moy)
-    GUI.build_map(0)
+    geo = projet.build_stations_coordonates("emplacement-des-gares-idf.csv")
+    md = projet.build_map_data(geo,moy)
+    GUI.build_map(md)
     # print (md)
     pass
 
